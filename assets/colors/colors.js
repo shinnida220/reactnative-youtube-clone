9556eb8b11e508eb132e434b86735cd25547e8b8
Nuel <ibk.com@gmail.com>
2023-04-22 21:30:29 +0100

    setup folder structure, added new splash image

---
+++ assets/colors/colors.js
@@ -0,0 +1,15 @@
+export default colors = {
+	black: "#0A0A0A",
+	lighteBlack: "#3B3B3B",
+	gray: "#6C6C6C",
+	lightGray: "#6C6C6C",
+	lighterGray: "#6C6C6C",
+	red: "#FF0000",
+	redV2: "#FF3232",
+	redV3: "#CD0000",
+	darkerRed: "#680000",
+	darkRed: "#9A0000",
+	lightRed: "#FF6565",
+	lighterRed: "#FF9797",
+	lightestRed: "#FFCACA",
+};
